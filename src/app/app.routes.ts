46feb3f56f82109c14ad446455c7425532239b7d
Jonathan Khor <jonathan.khor@cogfs.com.au>
2024-03-27 21:17:44 +0800

    msg

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {FirstComponent} from './first/first.component';
+import {SecondComponent} from './second/second.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'first-component', component: FirstComponent },
+    { path: 'second-component', component: SecondComponent },
+    { path: 'yo', redirectTo: '/first-component', pathMatch: 'prefix' },
+    { path: '**', component: PageNotFoundComponent }
+];
